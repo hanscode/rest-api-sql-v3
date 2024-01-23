6bc4d97b09ab6054322149fd7d0e0c27c29b650c
Hans Steffens <hans@steffens.co>
2024-01-23 15:52:39 -0500

    Model Associations Defined

---
+++ models/course.js
@@ -0,0 +1,31 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  class Course extends Model {}
+  User.init({
+    title: {
+      type: DataTypes.STRING
+    },
+    description: {
+      type: DataTypes.TEXT    
+    },
+    estimatedTime: {
+      type: DataTypes.STRING
+    },
+    materialsNeeded: {
+      type: DataTypes.STRING
+    }
+  }, { sequelize });
+
+  Course.associate = (models) => {
+    // Tells Sequelize that a course can be associated with only 1 user
+    Course.belongsTo(models.User, {
+        foreignKey: {
+            fieldName: 'userId',
+        },
+    });
+};
+
+  return Course;
+};
