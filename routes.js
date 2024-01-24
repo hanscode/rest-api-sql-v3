3ef71359b1eaa8cb2d221e24cf44cbf489282e99
Hans Steffens <hans@steffens.co>
2024-01-24 12:59:14 -0500

    Extra Credits Done

--- routes.js
+++ routes.js
@@ -15,7 +15,9 @@ const router = express.Router();
 
 // Route that returns a list of users.
 router.get("/users", authenticateUser, asyncHandler(async (req, res) => {
-    let users = await User.findAll();
+    let users = await User.findAll({
+      attributes: ['id', 'firstName', 'lastName', 'emailAddress']
+    });
     res.json(users);
   })
 );
@@ -51,9 +53,11 @@ router.post("/users", asyncHandler(async (req, res) => {
 // Send a GET request to /courses to return all courses
 router.get("/courses", asyncHandler(async (req, res) => {
     const courses = await Course.findAll({
+      attributes: ['id', 'title', 'description', 'estimatedTime', 'materialsNeeded', 'userId'],
       include: [
         {
           model: User,
+          attributes: ['id', 'firstName', 'lastName', 'emailAddress']
         },
       ],
     });
@@ -63,7 +67,10 @@ router.get("/courses", asyncHandler(async (req, res) => {
 
 // GET individual course detail
 router.get("/courses/:id", asyncHandler(async (req, res, next) => {
-    const course = await Course.findByPk(req.params.id);
+    const course = await Course.findOne({
+      attributes: ['id', 'title', 'description', 'estimatedTime', 'materialsNeeded', 'userId'],
+      where: { id: req.params.id }
+    });
     if (course) {
       res.status(200).json(course);
     } else {
@@ -98,13 +105,24 @@ router.post( "/courses", authenticateUser, asyncHandler(async (req, res) => {
 // PUT route that will update the corresponding course and return a 204 HTTP status code and no content.
 router.put('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
   let course;
+  // retrieve the current authenticated user's information from the Request object's `currentUser` property:
+  const user = req.currentUser;
   try {
     course = await Course.findByPk(req.params.id);
     if (course) {
-      // update the course object from the request body
-      await course.update(req.body);
-      // Send status 204 (meaning no content == everything went OK but there's nothing to send back)
-      res.status(204).end();
+      // Confirming that the currently authenticated user is the owner of the requested course.
+      const courseOwner = course.userId;
+      const authenticatedUser = user.id;
+
+      if (courseOwner === authenticatedUser) {
+          // update the course object from the request body
+          await course.update(req.body);
+          // Send status 204 (meaning no content == everything went OK but there's nothing to send back)
+          res.status(204).end();
+      } else {
+        res.status(403).json({message: "User is not owner of the requested course"});
+      }
+
     } else {
       res.status(400).json({message: "Course not found"});
     }
@@ -122,13 +140,23 @@ router.put('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
 // DELETE route that will delete the corresponding course and return a 204 HTTP status code and no content.
 router.delete('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
   let course;
+  // retrieve the current authenticated user's information from the Request object's `currentUser` property:
+  const user = req.currentUser;
   try {
     course = await Course.findByPk(req.params.id);
     if (course) {
-      // Delete the course object
-      await course.destroy();
-      // Send status 204 (meaning no content == everything went OK but there's nothing to send back)
-      res.status(204).end();
+      // Confirming that the currently authenticated user is the owner of the requested course.
+      const courseOwner = course.userId;
+      const authenticatedUser = user.id;
+
+      if (courseOwner === authenticatedUser) {
+        // Delete the course object
+        await course.destroy();
+        // Send status 204 (meaning no content == everything went OK but there's nothing to send back)
+        res.status(204).end();
+      } else {
+        res.status(403).json({message: "User is not owner of the requested course"});
+      }
     } else {
       res.status(400).json({message: "Course not found"});
     }
